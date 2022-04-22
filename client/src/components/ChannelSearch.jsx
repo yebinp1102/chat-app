9374a712bf493d883cc85887f13f1e4c17a54045
yebin Park <yebinp1102@naver.com>
2022-04-22 14:34:17 +0900

    create search function

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { useChatContext } from 'stream-chat-react'
 import {BiSearchAlt2} from 'react-icons/bi'
+import {ResultsDropdown} from './'
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+  const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState('')
   const [loading, setLoading] = useState(false)
+  const [teamChannels, setTeamChannels] = useState([])
+  const [directChannels, setDirectChannels] = useState([])
 
-  const getChannels = async(text) => {
+  useEffect(()=>{
+    if(!query){
+      setTeamChannels([])
+      setDirectChannels([])
+    }
+  },[query])
+
+  const getChannels = async (text) => {
     try{
-      // 채널 연결
-      
+      const channelResponse = client.queryChannels({
+        type: 'team',
+        name: { $autocomplete: text },
+        members: { $in: [client.userID]}
+      })
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },   // 검색 결과에서 자기 자신의 ID는 제외시킨다.
+        name: { $autocomplete: text }
+      })
+
+      const [channels, { users }] = await Promise.all([channelResponse, userResponse])
+      if(channels.length) setTeamChannels(channels)
+      if(users.length) setDirectChannels(users)
     }catch(err){
       setQuery('')
     }
@@ -20,6 +42,11 @@ const ChannelSearch = () => {
     setLoading(true)
     setQuery(e.target.value)
     getChannels(e.target.value)
+  }
+
+  const setChannel = (channel) => {
+    setQuery('')
+    setActiveChannel(channel)
   }
 
   return (
@@ -33,6 +60,17 @@ const ChannelSearch = () => {
           onChange={onSearch}
         />
       </div>
+      { query && (
+        // ResultsDropdown 컴포넌트는 모든 채널과 유저에 대한 모든 정보를 갖고 있다.
+        <ResultsDropdown
+          teamChannels={teamChannels}
+          directChannels={directChannels}
+          loading={loading}
+          setChannel={setChannel}
+          setQuery={setQuery}
+          setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   )
 }
